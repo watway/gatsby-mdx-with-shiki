d854546bae0d430ca806e3ba4c8c9d3f093358a0
watway <craig@watway.net>
2021-11-18 08:14:51 +1000

    add mdx plugin and remark-shiki

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,26 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/pages`,
+      },
     },
-    plugins: [
-
-    ]
-}
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-shiki',
+            options: {
+              theme: 'nord',
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
